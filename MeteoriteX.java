19e4315c69052cba6b096f13dd6ea28ea1515b80
Munbod <Natakrit120@gmail.com>
2024-09-17 17:33:45 +0700

    add direction movement

--- MeteoriteX.java
+++ MeteoriteX.java
@@ -3,6 +3,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.JFrame;
@@ -20,14 +21,13 @@ class MyFrame extends JFrame {
 
     public MyFrame() {
         setTitle("MeteoriteX");
-        setSize(800, 800);
+        setSize(900, 900);
         setLayout(null);
-        setBackground(Color.black);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         add(myGraphics);
-
+        
         setVisible(true);
     }
 }
@@ -35,15 +35,15 @@ class MyFrame extends JFrame {
 class MyGraphics extends JPanel {
     
     Image meteo = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + "materials\\1.png");
-    int x = 0;
-    int y = 0;
+    int x = 1;
+    int y = 1;
 
     public MyGraphics() {
         setSize(800, 800);
         setLocation(0, 0);
         
         Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(this), 0, 10);
+        timer.scheduleAtFixedRate(new MyTimerTask(this), 0, 5);
     }
 
     @Override
@@ -58,6 +58,9 @@ class MyGraphics extends JPanel {
 class MyTimerTask extends TimerTask {
 
     MyGraphics graphics;
+    int movementX = 1;
+    int movementY = 1;
+    int direction = new Random().nextInt(3) + 1;
 
     public MyTimerTask(MyGraphics graphics) {
         this.graphics = graphics;
@@ -65,7 +68,33 @@ class MyTimerTask extends TimerTask {
 
     @Override
     public void run() {
-        graphics.x++;
+        if (direction == 1) {
+            graphics.x = graphics.x + movementX;
+
+            if (graphics.x > 740 || graphics.x < 0) {
+                movementX = movementX * -1;
+                direction = new Random().nextInt(3) + 1;
+            }
+        }
+        else if (direction == 2) {
+            graphics.y = graphics.y + movementY;
+
+            if (graphics.y > 740 || graphics.y < 0) {
+                movementY = movementY * -1;
+                direction = new Random().nextInt(3) + 1;
+            }
+        }
+        else if (direction == 3) {
+            graphics.x = graphics.x + movementX;
+            graphics.y = graphics.y + movementY;
+
+            if (graphics.x > 740 || graphics.x < 0 || graphics.y > 740 || graphics.y < 0) {
+                movementX = movementX * -1;
+                movementY = movementY * -1;
+                direction = new Random().nextInt(3) + 1;
+            }
+        }
+
         graphics.repaint();
     }
 }
