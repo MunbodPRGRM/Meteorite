ce6e7ceed22c42c3e9749e26127fc05e4955d792
Munbod <Natakrit120@gmail.com>
2024-09-22 22:33:49 +0700

    a meteo change speed

--- MeteoriteX.java
+++ MeteoriteX.java
@@ -16,12 +16,11 @@ public class MeteoriteX {
 }
 
 class MyFrame extends JFrame {
-
     MyGraphics myGraphics = new MyGraphics();
 
     public MyFrame() {
         setTitle("MeteoriteX");
-        setSize(900, 900);
+        setSize(812, 828);
         setLayout(null);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,69 +32,96 @@ class MyFrame extends JFrame {
 }
 
 class MyGraphics extends JPanel {
-    
-    Image meteo = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + "materials\\1.png");
-    int x = 1;
-    int y = 1;
+    Image[] meteorites = new Image[1];
+    int[] x = new int[meteorites.length];
+    int[] y = new int[meteorites.length];
+    int[] xMovement = new int[meteorites.length];
+    int[] yMovement = new int[meteorites.length];
+    int[] speed = new int[meteorites.length];
+    int[] direction = new int[meteorites.length];
+    int[] count = new int[meteorites.length];
+    Timer[] timers = new Timer[meteorites.length];
 
     public MyGraphics() {
         setSize(800, 800);
         setLocation(0, 0);
-        
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(this), 0, 1);
+
+        for (int i = 0; i < meteorites.length; i++) {
+            meteorites[i] = Toolkit.getDefaultToolkit().createImage(System.getProperty("user.dir") + File.separator + ("materials\\" + (new Random().nextInt(10) + 1) + ".png"));
+            
+            x[i] = new Random().nextInt(700);
+            y[i] = new Random().nextInt(700);
+            xMovement[i] = 1;
+            yMovement[i] = 1;
+            speed[i] = new Random().nextInt(10) + 1;
+            direction[i] = new Random().nextInt(3) + 1;
+        }
+
+        for (int i = 0; i < meteorites.length; i++) {
+            timers[i] = new Timer();
+            timers[i].scheduleAtFixedRate(new MyTimerTask(this, speed[i]), 0, 1);
+        }
     }
 
     @Override
     public void paintComponent(Graphics g) {
         g.setColor(Color.black);
         g.fillRect(0, 0, 800, 800);
-        
-        g.drawImage(meteo, x, y, x + 60, y + 60, 0, 0, 300, 300, this);
+
+        for (int i = 0; i < meteorites.length; i++) {
+            if (direction[i] == 1) {
+                x[i] = x[i] + xMovement[i];
+
+                if (x[i] <= 0 || x[i] >= getWidth() - 60) {
+                    xMovement[i] = xMovement[i] * -1;
+                    direction[i] = new Random().nextInt(3) + 1;
+                    count[i] = 1;
+                }
+            }
+            
+            if (direction[i] == 2) {
+                y[i] = y[i] + yMovement[i];
+
+                if (y[i] <= 0 || y[i] >= getHeight() - 60) {
+                    yMovement[i] = yMovement[i] * -1;
+                    direction[i] = new Random().nextInt(3) + 1;
+                    count[i] = 1;
+                }
+            }
+            
+            if (direction[i] == 3) {
+                x[i] = x[i] + xMovement[i];
+                y[i] = y[i] + yMovement[i];
+
+                if (x[i] <= 0 || x[i] >= getWidth() - 60 || y[i] <= 0 || y[i] >= getHeight() - 60) {
+                    xMovement[i] = xMovement[i] * -1;
+                    yMovement[i] = yMovement[i] * -1;
+                    direction[i] = new Random().nextInt(3) + 1;
+                    count[i] = 1;
+                }
+            }
+
+            g.drawImage(meteorites[i], x[i], y[i], x[i] + 60, y[i] + 60, 0, 0, 300, 300, this);
+        }
     }
 }
 
 class MyTimerTask extends TimerTask {
-
     MyGraphics graphics;
-    int movementX = 1;
-    int movementY = 1;
-    int direction = new Random().nextInt(3) + 1;
-    int speed = new Random().nextInt(5) + 1;
+    int speed = 0;
 
-    public MyTimerTask(MyGraphics graphics) {
+    public MyTimerTask(MyGraphics graphics, int speed) {
         this.graphics = graphics;
+        this.speed = speed;
     }
 
     @Override
     public void run() {
-        if (direction == 1) {
-            graphics.x = graphics.x + movementX;
-
-            if (graphics.x > 740 || graphics.x < 0) {
-                direction = new Random().nextInt(3) + 1;
-                speed = new Random().nextInt(5) + 1;
-                movementX = movementX * -1;
-            }
-        }
-        else if (direction == 2) {
-            graphics.y = graphics.y + movementY;
-
-            if (graphics.y > 740 || graphics.y < 0) {
-                direction = new Random().nextInt(3) + 1;
-                speed = new Random().nextInt(5) + 1;
-                movementY = movementY * -1;
-            }
-        }
-        else if (direction == 3) {
-            graphics.x = graphics.x + movementX;
-            graphics.y = graphics.y + movementY;
-
-            if (graphics.x > 740 || graphics.x < 0 || graphics.y > 740 || graphics.y < 0) {
-                direction = new Random().nextInt(3) + 1;
-                speed = new Random().nextInt(5) + 1;
-                movementX = movementX * -1;
-                movementY = movementY * -1;
+        for (int i = 0; i < graphics.meteorites.length; i++) {
+            if (graphics.count[i] == 1) {
+                speed = new Random().nextInt(10) + 1;
+                graphics.count[i] = 0;
+                System.out.println(speed);
             }
         }
 
@@ -103,6 +129,6 @@ class MyTimerTask extends TimerTask {
 
         try {
             Thread.sleep(speed);
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException ex) {}
     }
 }
