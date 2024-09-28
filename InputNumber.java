8d3a35718bb2deea71b95a4c08ca802ffff0c78c
Munbod <Natakrit120@gmail.com>
2024-09-28 20:26:29 +0700

    add commemt

--- InputNumber.java
+++ InputNumber.java
@@ -6,12 +6,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
+// class main ใช้รันเพื่อแสดง frame ที่สามารถ input จำนวนอุกกาบาต
 public class InputNumber {
     public static void main(String[] args) {
         InputFrame frame = new InputFrame();
     }
 }
 
+// class frame ที่ตั้งค่าขนาดต่างๆ ของ frame
 class InputFrame extends JFrame implements ActionListener {
     JTextField textField = new JTextField();
     JButton button = new JButton("OK");
@@ -24,6 +26,7 @@ class InputFrame extends JFrame implements ActionListener {
 
         add(textField, BorderLayout.CENTER);
         add(button, BorderLayout.SOUTH);
+        button.setFocusable(false);
         button.addActionListener(this);
 
         setVisible(true);
@@ -31,10 +34,11 @@ class InputFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // ถ้ากดที่ปุ่ม button จะเป็นการเรียก frame ที่มีอุกกาบาตเคลื่อนที่ไปมา
         if (e.getSource() == button) {
             int number = Integer.parseInt(textField.getText());
-
             MyFrame myFrame = new MyFrame(number);
+            
             dispose();
         }
     }
--- MeteoriteX.java
+++ MeteoriteX.java
@@ -13,17 +13,20 @@ import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
+// class main ใช้รันโดยที่ไม่ได้ input ค่า
 public class MeteoriteX {
     public static void main(String[] args) {
         MyFrame frame = new MyFrame(2);
     }
 }
 
+// class frame ใช้แสดงผล frame ตามค่าที่ตั้งไว้
 class MyFrame extends JFrame {
+    // constructor ที่ต้องมีการ input number ก่อน ถึงจะเรียกใช้งานได้
     public MyFrame(int number) {
+        // เรียกใช้ MyPaint โดยใส่ค่า number ด้วย
         MyPaint paint = new MyPaint(number);
-        paint.number = number;
+        paint.number = number;          // มีปัญหาที่ใส่ค่า number ลงใน parameter แล้วค่าไม่ไป เลยเรียกใช้โดยตรง
 
         setTitle("Meteorite X");
         setSize(800, 800);
@@ -36,23 +39,27 @@ class MyFrame extends JFrame {
     }
 }
 
+// class paint ใช้เพื่อเรียกคำสัััั่งวาดรูป
 class MyPaint extends JPanel {
-    int number;
-    int size = 50;
-    Image[] meteors;
-    Image[] bomb;
-    Random rand = new Random();
-    int[] x;
-    int[] y;
-    int[] dx;
-    int[] dy;
-    int[] direction;
-    boolean[] meteoAlive;
-    boolean[] bombAlive;
-    MyThread[] threads;
-    Timer[] timers;
+    // ประกาศตัวแปรเอาไว้ก่อน
+    int number;         // ตัวแปรที่จะเอาไว้กำหนดจำนวนอุกกาบาต
+    int size = 50;          // ขนาดของอุกกาบาต
+    Image[] meteors;            // รูปของอุกกาบาต
+    Image[] bomb;           // รูปของระเบิด
+    Random rand = new Random();         // object random จะได้ไม่ต้อง new ทุกครั้ง
+    int[] x;            // ตำแหน่ง x ของอุกกาบาต
+    int[] y;            // ตำแหน่ง y ของอุกกาบาต
+    int[] dx;           // การเคลื่อนที่ของอุกกาบาตในแนว x มี 1 ถึง 3 และ -1 ถึง -3
+    int[] dy;           // การเคลื่อนที่ของอุกกาบาตในแนว y มี 1 ถึง 3 และ -1 ถึง -3
+    int[] direction;            // ทิศทางการเคลื่อนที่ของอุกกาบาต มี 0 แนวนอน, 1 แนวตั้ง, 2 แนวทะแยง
+    boolean[] meteoAlive;           // ใช้ตรวจสอบว่าอุกกาบาตยังอยู่
+    boolean[] bombAlive;            // ใช้ตรวจสอบว่าระเบิดถูกใช้งาน
+    MyThread[] threads;         // สร้าง thread จาก class MyThread เพื่อทำให้อุกกาบาตดูเหมื่อนเคลื่อนที่
+    Timer[] timers;         // สร้าง timer เพื่อกำหนดระยะเวลาที่ระเบิดจะหายไป
     
+    // constructor ที่ต้องมีการ input number ก่อน ถึงจะเรียกใช้งานได้
     public MyPaint(int number) {
+        // กำหนดตัวแปรอาเรย์หลังจากมี number
         meteors = new Image[number];
         bomb = new Image[number];
         x = new int[number];
@@ -65,18 +72,23 @@ class MyPaint extends JPanel {
         threads = new MyThread[number];
         timers = new Timer[number];
 
+        // สร้าง panel เพื่อให้สามารถวาดรูปบน panel
         setSize(getWidth(), getHeight());
         setLocation(0, 0);
 
+        // ใส่ข้อมูลลงใน image และตัวแปรต่างๆ เพื่อเตรียมวาดรูป
         for (int i = 0; i < number; i++) {
+            // สร้างอุกกาบาต แล้วก็สุ่มรูปอุกกาบาต
             meteors[i] = Toolkit.getDefaultToolkit().createImage(
                 System.getProperty("user.dir") + File.separator + "materials\\" + (rand.nextInt(10) + 1) + ".png"
             );
 
+            // สร้างระเบิด เพื่อรอใช้งาน
             bomb[i] = Toolkit.getDefaultToolkit().createImage(
                 System.getProperty("user.dir") + File.separator + "materials\\bomb.gif"
             );
 
+            // กำหนดค่าต่างๆ เพื่อเตรียมใช้งาน
             x[i] = rand.nextInt(700);
             y[i] = rand.nextInt(700);
             dx[i] = rand.nextInt(3) + 1;
@@ -89,19 +101,21 @@ class MyPaint extends JPanel {
             threads[i].start();
         }
 
+        // ถ้าคลิกที่อุกกาบาต 2 ครั้ง จะทำให้อุกกาบาตหายไป
         addMouseListener(new MouseAdapter() {
 
             @Override
             public void mousePressed(MouseEvent e) {
                 if (e.getClickCount() == 2) {
                     for (int i = 0; i < number; i++) {
+                        // ตรวจว่าอุกกาบาตนั้นมีอยู่หรือไม่ และเรียกใช้ method ที่สามารถตรวจสอบขนาดสี่เหลี่ยมของอุกกาบาต
                         if (meteors[i] != null && getRectMeteor(x[i], y[i]).contains(e.getPoint())) {
                             dx[i] = 0;
                             dy[i] = 0;
                             meteoAlive[i] = false;
                             bombAlive[i] = true;
 
-                            explosionMeteos();
+                            explosionMeteos();          // method บรรทัดที่ 127 : เพื่อวาดรูประเบิด
                         }
                     }
                 }
@@ -109,10 +123,12 @@ class MyPaint extends JPanel {
         });
     }
 
+    // method ทีีใช้ timer ที่จะทำให้รูป gif ทำงานและหายไป
     public void explosionMeteos() {
         for (int i = 0; i < number; i++) {
             if (bombAlive[i]) {
                 timers[i] = new Timer();
+                // เรียกใช้ TimerTask แบบ inner class
                 timers[i].schedule(new TimerTask() {
 
                     @Override
@@ -128,50 +144,62 @@ class MyPaint extends JPanel {
         }
     }
 
+    // method paint ใช้วาดรูป
     @Override
     protected void paintComponent(Graphics g) {
+        // วาดรูปพื้นหลังสีดำ
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
 
+        // วาดรูปอุกกาบาต และระเบิด
         for (int i = 0; i < number; i++) {
+            // วาดรูปอุกกาบาต เมื่อระเบิดยังเป็น false
             if (meteoAlive[i] && !bombAlive[i]) {
                 g.drawImage(meteors[i], x[i], y[i], x[i] + size, y[i] + size, 0, 0, 300, 300, this);
             }
 
+            // วาดรูประเบิด เมื่อระเบิดเป็น true
             if (!meteoAlive[i] && bombAlive[i]) {
                 g.drawImage(bomb[i], x[i], y[i], x[i] + size, y[i] + size, 0, 0, 80, 80, this);
             }
         }
 
-        movingMeteor();
+        movingMeteor();         // method บรรทัดที่ 171 : เพื่อให้อุกกาบาตเคลื่อนที่ตามแนว
     }
 
+    // method ที่จะควบคุมลักษณะการเคลื่อนที่ของอุกกาบาต
     public void movingMeteor() {
         for (int i = 0; i < number; i++) {
+            // เคลื่อนที่แนวนอน
             if (direction[i] == 0) {
                 x[i] += dx[i];
             }
+            // เคลื่อนที่แนวตั้ง
             else if (direction[i] == 1) {
                 y[i] += dy[i];
             }
+            // เคลื่อนที่แนวเฉียง
             else if (direction[i] == 2) {
                 x[i] += dx[i];
                 y[i] += dy[i];
             }
 
-            checkWallHit();
-            checkMeteosHit();
+            checkWallHit();         // method บรรทัดที่ 193 : เพื่อตรวจสอบเมื่ออุกกาบาตชนเข้ากับขอบของ frame
+            checkMeteosHit();           // method บรรทัด 212 : เพื่อตรวจสอบเมื่ออุกกาบาต 2 ลูกมาชนกัน
         }
     }
 
+    // method ตรวจสอบการชนกับขอบของ frame
     public void checkWallHit() {
         for (int i = 0; i < number; i++) {
+            // เมื่อขอบชนแถวๆ ผั่งซ้าย
             if (x[i] <= 0 || y[i] <= 0) {
                 dx[i] = rand.nextInt(3) + 1;
                 dy[i] = dx[i];
                 direction[i] = rand.nextInt(3);
             }
 
+            // เมื่อขอบชนแถวๆ ผั่งขวา
             if (x[i] >= getWidth() - size || y[i] >= getHeight() - size) {
                 dx[i] = rand.nextInt(3) - 3;
                 dy[i] = dx[i];
@@ -180,25 +208,31 @@ class MyPaint extends JPanel {
         }
     }
 
+    // method ตรวจสอบเมื่ออุกกาบาต 2 ลูกมาชนกัน
     public void checkMeteosHit() {
         for (int i = 0; i < number; i++) {
             for (int j = i + 1; j < number; j++) {
+                // เรียกใช้ method getRectMeteor ทั้งสองลูก เทื่อตรวจสอบว่ามีการซ้อนทับกันของตำแหน่งหรือไม่
                 if (getRectMeteor(x[i], y[i]).intersects(getRectMeteor(x[j], y[j]))) {
+                    // จะทำงานก็ต่อเมื่ออุกกาบาตทั้ง 2 ลูกยังไม่ระเบิด
                     if (meteoAlive[i] && meteoAlive[j]) {
-                        if ((dx[i] < 0 && dy[i] < 0) || (dx[j] < 0 && dy[j] < 0)) {
+                        // ถ้าการเคลื่อนที่ของอุกกาบาตทั้งสองเป็นลบ ทำการสุ่มการเคลื่อนที่
+                        if (dx[i] < 0 || dy[i] < 0 || dx[j] > 0 || dy[j] > 0) {
                             dx[i] = rand.nextInt(3) - 3;
                             dy[i] = dx[i];
                             dx[j] = rand.nextInt(3) + 1;
                             dy[j] = dx[j];
                         }
 
-                        if ((dx[i] > 0 && dy[i] > 0) || (dx[j] > 0 && dy[j] > 0)) {
+                        // ถ้าการเคลื่อนที่ของอุกกาบาตทั้งสองเป็นบวก ทำการสุ่มการเคลื่อนที่
+                        if (dx[i] > 0 || dy[i] > 0 || dx[j] < 0 || dy[j] < 0) {
                             dx[i] = rand.nextInt(3) - 3;
                             dy[i] = dx[i];
                             dx[j] = rand.nextInt(3) + 1;
                             dy[j] = dx[j];
                         }
                         
+                        // สุ่มลักษณะการเคลื่อนที่ใหม่
                         direction[i] = rand.nextInt(3);
                         direction[j] = rand.nextInt(3);
                     }
@@ -207,12 +241,15 @@ class MyPaint extends JPanel {
         }
     }
 
+    // method ที่กำหนดลักษณะสี่เหลี่ยมของอุกกาบาต
     public Rectangle getRectMeteor(int x, int y) {
         return new Rectangle(x, y, size, size);
     }
 }
 
+// class thread เอาไว้ทำให้อุกกาบาตดูเหมือนเคลื่อนที่
 class MyThread extends Thread {
+    // เรียกใช้ class MyPaint เพื่อที่จะวาดลง panel เรื่อยๆ
     MyPaint paint;
 
     public MyThread(MyPaint paint) {
